b22763d8f71ef4b3df88624ddd5a5474335f51a4
takkar <yurisuki915@gmail.com>
2022-08-07 11:59:42 +0900

    Home画面追加

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,14 +3,66 @@ import profileImage from "../Images/profile.jpg";
 
 const HomePage = () => {
   return (
-    <div className="container text-center">
-    <h1>About me</h1>
+    <div>
+      <section className='about-me'>
+        <h1>プロフィール</h1>
+        <div class="profile">
+          {/* <img src={profileImage} className="profileImage" /> */}
+          <div class="profile__name">Takanobu Harada</div>
+          <div class="profile__desc">静岡大学 情報学部 3年。
+          Python/Java/Javascript/React/Django等を勉強中。
+          </div>
+        </div>      
+      </section>
 
-    {/* <img src={profileImage} className="profileImage" /> */}
+    <section className='career'>
+    <h1>経歴</h1>
+      <ul class="timeline">
+        <li>
+          <p class="timeline-date">2017年4月</p>
+          <div class="timeline-content">
+            <h3>都立小金井北高校入学</h3>
+            <p></p>
+          </div>
+        </li>
+        <li>
+          <p class="timeline-date">2020年3月</p>
+          <div class="timeline-content">
+            <h3>都立小金井北高校卒業</h3>
+            <p></p>
+          </div>
+        </li>
+        <li>
+          <p class="timeline-date">2020年4月</p>
+          <div class="timeline-content">
+            <h3>静岡大学情報学部入学</h3>
+            <p></p>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section className="about-certification">
+      <h1>資格</h1>
+      <div className='certification'>
+        <h3>取得済み</h3>
+      <ul>
+        <li>TOEIC L&R 850点 (Listening 435+Reading 415、2022年5月 公開テスト) </li>
+        <li>実用英語技能検定 準1級 (2021年6月) </li>
+        <li>普通自動車第一種運転免許(AT限定)</li>
+      </ul>
+        <h3>勉強中</h3>
+        <ul>
+        <li>基本情報技術者試験 (2022年1月受験予定)</li>
+        <li>実用英語技能検定 1級 (2022年10月受験予定)</li>
+      </ul>
+      </div>
+
+    </section>
 
     
-
     
+      
     
   </div>
   )
--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,53 +1,11 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import {Link} from 'react-router-dom';
 
 const WorkPage = () => {
   return (
-    <div class='container text-center'>
-      <h2 class="section-heading text-uppercase" style={{margin : "100px"}}>My works</h2>
-          <h3 class="section-subheading text-muted mb-5">
-            私が作った作品一覧です
-          </h3>
+    <div>
+      
 
-    <section class="page-section" id="services">
-      <div class="row" style={{margin: "0 auto"}}>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>ポートフォリオサイト</Card.Title>
-        <Card.Text>
-          Reactを使い、このポートフォリオサイトを作成しました。
-        </Card.Text>
-        <Button variant="outline-primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>楽天クレジットカード明細分析ツール</Card.Title>
-        <Card.Text>
-          Pythonを用い、csv形式の楽天クレジットカード明細を分析する実行ファイルを作成しました。
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="../Images/profile.jpg" />
-      <Card.Body>
-        <Card.Title>オリジナル単語帳作成アプリ
-        </Card.Title>
-        <Card.Text>
-          Djangoを用い、オリジナルの英単語帳を作成できるアプリを作成しました。
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-
-      </div>
-     </section>
-     </div>
+    </div>
   )
 }
 
